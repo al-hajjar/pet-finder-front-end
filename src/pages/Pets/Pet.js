420ee21a6feff2a5a3c12962112ababfb93029c3
kidhri salim <salimkhidhri@gmail.com>
2023-03-07 19:41:52 +0300

    added pet.js addpet.js

--- src/pages/Pets/Pet.js
+++ src/pages/Pets/Pet.js
@@ -1,105 +1,93 @@
-import { useState } from "react"
+import { useEffect, useState, useContext } from "react";
+import { useNavigate} from "react-router-dom";
 import { apiHost } from "../../Variables";
 
 
-const AddPet = () => {
-    const [petFormData, setPetFormData] = useState(
-        {
-            name: "",
-            breed: "",
-            image_url: "",
-            user_id: JSON.parse(localStorage.getItem('user') || false)?.id
+const Pets = ({loggedIn}) => {
+    const navigate = useNavigate()
+    const [pets, setPets] = useState([])
+
+    useEffect(()=>{
+        if(!loggedIn){
+            navigate('/home')
         }
-    )
+    }, [])
 
-    function updateFormData(e){
+    useEffect(()=>{
+        fetch(`${apiHost}/pets`)
+            .then((res) => res.json())
+            .then((pets) => {
+                setPets(pets)
+                console.log(pets)
+            })   
+    }, [])
 
-        setPetFormData(petFormData => {
-            return {...petFormData, [e.target.id]: e.target.value}
-        })
+    // useEffect(()=>{
+    //     fetch(`${apiHost}/${`my-pets/${JSON.parse(localStorage.getItem('user') || false)?.id}`}`)
+    //         .then((res) => {
+    //             if(res.ok){
+    //                 res.json().then(data => setPets(data))
+    //             }else {
+    //                 res.json().then(error => console.warn(error))
+    //             }
+    //         })    
+    // }, [])
 
-    }
-
-    const handleForm = (e) => {
-        e.preventDefault();
-        console.log(petFormData)
-
-        fetch(`${apiHost}/pets`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(petFormData)
-        })
-        .then(res => {
+    function handleDelete(deletedPet){
+        fetch(`${apiHost}/pets/${deletedPet.id}`, {method: 'DELETE'})
+        .then((res) => {
             if(res.ok){
-                res.json().then(data => {
-                    setPetFormData(
-                        {
-                            name: "",
-                            breed: "",
-                            image_url: "",
-                            user_id: JSON.parse(localStorage.getItem('user') || false)?.id
-                        }
-                    )
-                })
-            }else {
+                const newPetsList = pets.filter(pet => pet.id !== deletedPet.id)
+                setPets(newPetsList)
+            } else {
                 res.json().then(error => console.warn(error))
-            }
-        })
+            }   
+        }) 
     }
 
     return ( 
-        <>
-            <div className="flex flex-col justify-center items-center min-h-screen md:my-8 my-4 mx-6">
-                <h1 className="font-bold uppercase md:text-2xl text-xl text-gray-800 md:mb-16 mb-8">Add New Porject</h1>
-                <form onSubmit={handleForm}>
-                    <div>
-                        <label className='form-label'> 
-                            Pet name
-                        </label>
-                        <input 
-                            id="name"
-                            type="text"  
-                            class="input-form"
-                            placeholder="Pet name"
-                            value={petFormData.name}
-                            onChange={updateFormData} 
-                        />
+        <div className="min-h-screen md:px-24 px-8 py-20">
+            <div className="flex flex-col relative">
+                <div className="flex justify-between items-center my-5">
+                    <h1 className="md:text-2xl text-xl font-bold">ALL PETS</h1>
+                    <div className="flex gap-5">
+                        <button onClick={()=>navigate('/add-pet')}
+                            className="border-solid border border-blue py-2 px-5 w-40 rounded-md bg-pink-400 hover:bg-pink-500">
+                            Add New
+                        </button>
                     </div>
-                    <div>
-                        <label className='form-label'> 
-                            Breed
-                        </label>
-                        <input 
-                            id="breed"
-                            type="text" 
-                            class="input-form"
-                            placeholder="Breed"
-                            value={petFormData.breed}
-                            onChange={updateFormData} 
-                        />
-                    </div>
-                    <div>
-                        <label className='form-label'> 
-                            Image URL
-                        </label>
-                        <input 
-                            id="image_url"
-                            class="textarea textarea-info w-full my-4" 
-                            rows='3'
-                            placeholder="Image URL"
-                            value={petFormData.image_url}
-                            onChange={updateFormData}
-                        />
-                    </div>
-                    <button className="btn btn-secondary w-full">
-                        Submit
-                    </button>
-                </form>
+                </div>
+                <table>
+                    {
+                        <tr className="min-w-max-content border-x-solid border border-sky">
+                            <th>Name</th>
+                            <th>Breed</th>
+                            <th>Image</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    }
+                    {pets.map((pet) => (
+                            <tr key={pet.id} className="border-x-solid border border-sky">
+                                <td className="px-3" >{pet.name}</td>
+                                <td className="px-3">{pet.breed}</td>
+                                <td className="px-3 max-w-sm">
+                                    <img src={pet.image_url} alt='pet-image'/>
+                                </td>
+                                <td className="px-5">
+                                    <button 
+                                        className="border-solid border border-blue py-2 px-4 rounded-md bg-green-300 hover:bg-green-400 w-full" 
+                                        onClick={()=>handleDelete(pet)}
+                                    >
+                                        Delete
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                </table>
             </div>
-        </>
-     );
+        </div>
+    );
 }
  
-export default AddPet;
+export default Pets;
