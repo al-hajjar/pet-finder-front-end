9a28374368b4daffb597d3a469f5e0707f879314
kidhri salim <salimkhidhri@gmail.com>
2023-03-07 19:43:23 +0300

    completed home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import Breeds from "../components/Breeds";
+import Hero from "../components/Hero";
+
+const Home = () => {
+    return ( 
+        <>
+            <Hero />
+            <Breeds />
+        </>
+     );
+}
+ 
+export default Home;
